8078b8fc3d9955d8f35c35f13ec5b87967fba791
vedantika varnekar <varnekarvedantika.com>
2026-01-06 23:57:16 +0530

    trainer evaluation and mlflow committed

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -9,7 +9,7 @@ from networksecurity.constants.traning_pipeline import TARGET_COLUMN
 from networksecurity.constants.traning_pipeline import DATA_TRANSFORMATION_IMPUTER_PARAMS
 
 from networksecurity.entity.artifact_entity import (
-    DataTransformationartifact,
+    DataTransformationArtifact,
     DataValidationArtifact
 )
 
@@ -48,7 +48,7 @@ class DataTransformation:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
 
-    def initiate_data_transformation(self)->DataTransformationartifact:
+    def initiate_data_transformation(self)->DataTransformationArtifact:
         logging.info("Entered initiate-data_transformation method of DataTransformation class")
         try:
             logging.info("Starting data transformation")
@@ -76,7 +76,7 @@ class DataTransformation:
             save_numpy_array_data(self.data_transformation_config.transformed_test_file_path,array=test_arr,)
             save_object(self.data_transformation_config.transformed_object_file_path,preprocessor_object,)
         
-            data_transformation_artifact=DataTransformationartifact(
+            data_transformation_artifact=DataTransformationArtifact(
                 transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
                 transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
                 transformed_test_file_path=self.data_transformation_config.transformed_test_file_path
